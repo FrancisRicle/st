80ad24e0546fd545baf83c5d4f79c3dbf75e2f6a
Francis Ricle <francisricleruiz@gmail.com>
2025-04-10 16:49:15 -0300

    flake pname

--- flake.nix
+++ flake.nix
@@ -16,12 +16,12 @@
     ];
     in {
       packages.${system}.st = pkgs.st.overrideAttrs (old: {
-        pname = "st-myfork";
+        pname = "st";
         src = pkgs.fetchFromGitHub {
           owner = "FrancisRicle"; # Cambia esto
           repo = "st";
-          rev = "ab180974d910596ad936a291d3eeaefdb5b1b31c"; # Específico a tu versión
-          sha256 = "sha256-WcYoP3IXSes4QGeLBR4Nymmc8VL5COBSPaPRl554WQk="; # Reemplázalo con el hash correcto
+          rev = "3e7db675697a422170196ccdf4e3746024c9abed";
+          sha256 = "sha256-WcYoP3IXSes4QGeLBR4Nymmc8VL5COBSPaPRl554WQk=";
         };
         buildInputs = old.buildInputs ++ deps; # Agrega dependencias si es necesario
       });
